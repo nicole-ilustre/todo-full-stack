d0b674db9e62282b9f63892a92f65b5a4ee40d8d
Joshua Vial <joshua@enspiral.com>
2020-11-25 10:35:24 +1300

    api route for get todos

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-const { saveTodo } = require('../db')
+const { saveTodo, getTodos } = require('../db')
 
 router.post('/', (req, res) => {
   saveTodo(req.body.task)
-    .then(newTodo => {
-      res.status(201).json(newTodo)
-      return null
-    })
-    .catch(() => {
-      res.sendStatus(500)
-    })
+    .then(newTodo => res.status(201).json(newTodo))
+    .catch(() => res.sendStatus(500))
+})
+
+router.get('/', (req, res) => {
+  getTodos()
+    .then(todos => res.json(todos))
+    .catch(() => res.sendStatus(500))
 })
 
 module.exports = router
--- server/server.test.js
+++ server/server.test.js
@@ -1,20 +1,23 @@
 import request from 'supertest'
 import server from './server'
-import { saveTodo } from './db'
+import { saveTodo, getTodos } from './db'
 
-const fakeTodo = {
-  id: 1,
-  task: 'do things',
-  priority: null,
-  completed: false
-}
 jest.mock('./db', () => ({
-  saveTodo: jest.fn(() => Promise.resolve(fakeTodo))
+  saveTodo: jest.fn(),
+  getTodos: jest.fn()
 }))
 
+let promise
 describe('POST /api/v1/todos', () => {
-  let promise
+  const fakeTodo = {
+    id: 1,
+    task: 'do things',
+    priority: null,
+    completed: false
+  }
+
   beforeAll(() => {
+    saveTodo.mockImplementation(() => Promise.resolve(fakeTodo))
     promise = request(server)
       .post('/api/v1/todos')
       .send({ task: 'new task' })
@@ -51,6 +54,43 @@ describe('POST /api/v1/todos', () => {
       return request(server)
         .post('/api/v1/todos')
         .send({ task: 'new task' })
+        .then(res => {
+          expect(res.status).toBe(500)
+          return null
+        })
+    })
+  })
+})
+
+describe('GET /api/v1/todos', () => {
+  describe('when database call works', () => {
+    const fakeTodos = [{ task: 'thing' }, { task: 'another thing' }]
+    beforeAll(() => {
+      getTodos.mockImplementation(() => Promise.resolve(fakeTodos))
+      promise = request(server)
+        .get('/api/v1/todos')
+    })
+
+    test('call db.getTodos', () => {
+      return promise.then(() => {
+        expect(getTodos).toHaveBeenCalled()
+        return null
+      })
+    })
+    test('return todos from database', () => {
+      return promise.expect(200).then(response => {
+        expect(response.body).toEqual(fakeTodos)
+        return null
+      })
+    })
+  })
+
+  describe('when database does not work', () => {
+    test('returns 500', () => {
+      expect.assertions(1)
+      const err = new Error('horrible things')
+      getTodos.mockImplementation(() => Promise.reject(err))
+      return request(server).get('/api/v1/todos')
         .then(res => {
           expect(res.status).toBe(500)
           return null
