e2480ce29f5e7bddb51302f40fac412c143cc14f
Joshua Vial <joshua@enspiral.com>
2020-11-25 18:13:29 +1300

    editing todo item

--- client/components/Todo.jsx
+++ client/components/Todo.jsx
@@ -2,24 +2,62 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { removeTodo, updateTodo } from '../actions'
 
-const Todo = ({ todo, dispatch }) => {
-  const handleCheck = (e) => {
+class Todo extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      edit: false,
+      task: props.todo.task
+    }
+  }
+
+  handleCheck = (e) => {
+    const { dispatch, todo } = this.props
     dispatch(updateTodo(todo.id, { completed: e.target.checked }))
   }
-  const handleClick = (e) => {
+
+  handleClick = (e) => {
+    const { dispatch, todo } = this.props
     dispatch(removeTodo(todo.id))
   }
-  const className = todo.completed ? 'completed' : ''
-  return (
-    <li className={className}>
-      <div className="view">
-        <input onChange={handleCheck} className="toggle" type="checkbox" />
-        <label>{todo.task}</label>
-        <button onClick={handleClick} className="destroy"></button>
-      </div>
-      {/* <input className="edit" value="Create a TodoMVC template" /> */}
-    </li>
-  )
+
+  toggleEdit = (e) => {
+    if (this.props.todo.completed) return
+    this.setState({ edit: !this.state.edit })
+  }
+
+  handleChange = (e) => {
+    this.setState({ task: e.target.value })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const { dispatch, todo } = this.props
+    dispatch(updateTodo(todo.id, { task: this.state.task }))
+    this.setState({ edit: false })
+  }
+
+  render () {
+    const { todo } = this.props
+    const { edit, task } = this.state
+    let className = todo.completed ? 'completed' : ''
+    if (!className && edit) className = 'editing'
+    return (
+      <li className={className}>
+        <div className="view">
+          <input onChange={this.handleCheck} className="toggle" type="checkbox" checked={todo.completed} />
+          {!edit && <label onClick={this.toggleEdit}>{todo.task}</label>}
+          <button onClick={this.handleClick} className="destroy"></button>
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          {edit && <input className="edit"
+            onChange={this.handleChange}
+            value={task} />
+          }
+        </form>
+      </li>
+    )
+  }
 }
 
 export default connect()(Todo)
--- client/components/Todo.test.jsx
+++ client/components/Todo.test.jsx
@@ -28,13 +28,6 @@ describe('<Todo />', () => {
     expect(li.className).toBe('')
   })
 
-  test('completed className on li', async () => {
-    todo.completed = true
-    cleanup()
-    render(<Provider store={fakeStore}><Todo todo={todo} /></Provider>)
-    const li = await screen.getByRole('listitem')
-    expect(li.className).toBe('completed')
-  })
   test('shows task in label', async () => {
     const label = await screen.getByText(todo.task)
     expect(label).not.toBeUndefined()
@@ -47,17 +40,71 @@ describe('<Todo />', () => {
     expect(removeTodo).toHaveBeenCalledWith(todo.id)
   })
 
-  test('checking toggle sends updateTask action, toggling completed', async () => {
+  test('checking toggle sends updateTask action', async () => {
     jest.clearAllMocks()
     const checkbox = await screen.getByRole('checkbox')
     fireEvent.click(checkbox)
     expect(fakeStore.dispatch).toHaveBeenCalled()
     expect(updateTodo.mock.calls[0][0]).toBe(todo.id)
     expect(updateTodo.mock.calls[0][1]).toEqual({ completed: true })
+  })
 
-    fireEvent.click(checkbox)
-    expect(fakeStore.dispatch).toHaveBeenCalledTimes(2)
-    expect(updateTodo.mock.calls[1][0]).toBe(todo.id)
-    expect(updateTodo.mock.calls[1][1]).toEqual({ completed: false })
+  describe('editing', () => {
+    test('input field is hidden when not editing', async () => {
+      const input = await screen.queryByRole('textbox')
+      expect(input).toBeNull()
+    })
+    test('clicking the label shows the edit field', async () => {
+      const label = await screen.getByText(todo.task)
+      fireEvent.click(label)
+      const input = await screen.getByRole('textbox')
+      expect(input).not.toBeNull()
+      const listitem = await screen.getByRole('listitem')
+      expect(listitem.className).toContain('editing')
+    })
+    test('submitting edit field dispatches an updateTodo action', async () => {
+      const awesomeTask = 'make a pen pineapple apple pen'
+      jest.clearAllMocks()
+      const label = await screen.getByText(todo.task)
+      fireEvent.click(label)
+      const input = await screen.getByRole('textbox')
+
+      fireEvent.change(input, { target: { value: awesomeTask } })
+      fireEvent.submit(input)
+      expect(fakeStore.dispatch).toHaveBeenCalled()
+      expect(updateTodo.mock.calls[0][0]).toBe(todo.id)
+      expect(updateTodo.mock.calls[0][1]).toEqual({ task: awesomeTask })
+    })
+  })
+
+  describe('a completed component', () => {
+    beforeEach(() => {
+      jest.clearAllMocks()
+      todo.completed = true
+      cleanup()
+      render(<Provider store={fakeStore}><Todo todo={todo} /></Provider>)
+    })
+
+    test('unchecking toggle sends updateTask action', async () => {
+      const checkbox = await screen.getByRole('checkbox')
+      fireEvent.click(checkbox)
+      expect(fakeStore.dispatch).toHaveBeenCalled()
+      expect(updateTodo.mock.calls[0][0]).toBe(todo.id)
+      expect(updateTodo.mock.calls[0][1]).toEqual({ completed: false })
+    })
+
+    test('set class to completed and check input', async () => {
+      const li = await screen.getByRole('listitem')
+      expect(li.className).toBe('completed')
+      const checkbox = await screen.getByRole('checkbox')
+      expect(checkbox.checked).toBe(true)
+    })
+
+    test('disable editing', async () => {
+      const label = await screen.getByText(todo.task)
+      fireEvent.click(label)
+      const listItem = await screen.getByRole('listitem')
+      expect(listItem.className).not.toContain('editing')
+    })
   })
 })
