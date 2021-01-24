4ccc85ea9c761eee91479517f99bd699c1224fe8
Don Smith <hi@dons.dev>
2021-01-24 22:07:56 +1300

    temporarily fix reducer error

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 
-// import stuff from './stuff'
+import stuff from './stuff'
 
 export default combineReducers({
-  // stuff
+  stuff
 })
---
+++ client/reducers/stuff.js
@@ -0,0 +1,7 @@
+const initialStuff = 0
+
+function stuff (state = initialStuff, action) {
+  return state
+}
+
+export default stuff
