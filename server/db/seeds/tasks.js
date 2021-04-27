e7abecf4ce48641014d0ed28048712f4aab189e1
Caleb Ion <calebionpersonal@gmail.com>
2021-04-27 16:40:28 +1200

    Fix Seed file

--- server/db/seeds/tasks.js
+++ server/db/seeds/tasks.js
@@ -3,7 +3,7 @@ exports.seed = function (knex) {
   return knex('todos').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('todos').insert([
         { id: 1, task: 'Dishes', priority: 10, completed: false },
         { id: 2, task: 'Laundry', priority: 4, completed: true },
         { id: 3, task: 'Book Tickets', priority: 6, completed: false }
