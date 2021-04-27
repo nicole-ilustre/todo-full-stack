75fc938f8bcdf639e441cd7adba7cb3b81a276fe
Caleb Ion <calebionpersonal@gmail.com>
2021-04-27 17:34:47 +1200

    Add update todo route

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -6,7 +6,7 @@ const { addTodo, getTodos, getTodo, deleteTodo, updateTodo } = require('../db/db
 router.get('/', (req, res) => {
   getTodos()
     .then((todos) => {
-      res.json(todos)
+      res.sendStatus(200).json(todos)
       return null
     })
     .catch((e) => {
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
     })
 })
 
-router.post('/', (req,res) => {
+router.post('/', (req, res) => {
   addTodo(req.body.task)
     .then((todo) => {
       res.sendStatus(201).json(todo)
@@ -27,7 +27,18 @@ router.post('/', (req,res) => {
     })
 })
 
-//updatetodo
+router.update('/:id', (req, res) => {
+  updateTodo(Number(req.params.id), req.body.update)
+    .then((todo) => {
+      res.sendStatus(200).json(todo)
+      return null
+    })
+    .catch(e => {
+      res.sendStatus(500)
+      console.log(e.message)
+    })
+})
+
 
 //deletetodo
 
