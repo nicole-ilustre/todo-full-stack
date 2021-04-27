d0a7b03ade0e84dcc0b20d59a827fec40f420aa5
Caleb Ion <calebionpersonal@gmail.com>
2021-04-27 16:50:14 +1200

    Add db functions

---
+++ server/db/db.js
@@ -0,0 +1,31 @@
+const database = require('./connection')
+
+const getTodos = (db = database) => {
+  return db('todos').select()
+}
+
+const getTodo = (id, db = database) => {
+  return db('todos').where(id, 'id').select().first()
+}
+
+const addTodo = (task, db = database) => {
+  return db('todos').insert({ task })
+    .then((id) => getTodo(id))
+}
+
+const deleteTodo = (id, db = database) => {
+  return db('todos').where(id, 'id').delete()
+}
+
+const updateTodo = (id, update, db = database) => {
+  return db('todos').where(id, 'id').update(update)
+    .then((id) => getTodo(id))
+}
+
+module.exports = {
+  getTodos,
+  getTodo,
+  addTodo,
+  deleteTodo,
+  updateTodo
+}
