9d2d89f9276e8c899808acd1b889fb053dfac1b1
Caleb Ion <calebionpersonal@gmail.com>
2021-04-27 17:31:47 +1200

    Add post todo route

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -6,7 +6,7 @@ const { addTodo, getTodos, getTodo, deleteTodo, updateTodo } = require('../db/db
 router.get('/', (req, res) => {
   getTodos()
     .then((todos) => {
-      res.json(todos).sendStatus(200)
+      res.json(todos)
       return null
     })
     .catch((e) => {
@@ -15,7 +15,17 @@ router.get('/', (req, res) => {
     })
 })
 
-//addtodo
+router.post('/', (req,res) => {
+  addTodo(req.body.task)
+    .then((todo) => {
+      res.sendStatus(201).json(todo)
+      return null
+    })
+    .catch(e => {
+      res.sendStatus(500)
+      console.log(e.message)
+    })
+})
 
 //updatetodo
 
