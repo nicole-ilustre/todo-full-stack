9881872c91b1ed0cc37c0a75a8afb59f7d56e971
ysabel-guiang <ysabelregino@yahoo.com>
2021-04-27 14:38:03 +1200

    incorporate html/css into react

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -12,12 +12,61 @@ function App (props) {
 
   return (
     <>
+    <section id="app" className="todoapp">
+
       <header className="header">
         <h1>todos</h1>
         <AddTodo />
       </header>
-      <section className="main"></section>
-      <footer className="footer"></footer>
+
+      <section className="main">
+        <input id="toggle-all" className="toggle-all" type="checkbox" />
+        <label for="toggle-all">Mark all as complete</label>
+        <ul className="todo-list">
+
+          <li className="completed">
+            <div className="view">
+              <input className="toggle" type="checkbox" checked />
+              <label>Taste JavaScript</label>
+              <button className="destroy"></button>
+            </div>
+            <input className="edit" value="Create a TodoMVC template" />
+          </li>
+
+          <li>
+            <div className="view">
+              <input className="toggle" type="checkbox" />
+              <label>Buy a unicorn</label>
+              <button className="destroy"></button>
+            </div>
+            <input className="edit" value="Rule the web" />
+          </li>
+
+        </ul>
+      </section>
+
+      <footer className="footer">
+        <span className="todo-count"><strong>0</strong> item left</span>
+        <ul className="filters">
+          <li>
+            <a class="selected" href="#/">All</a>
+          </li>
+          <li>
+            <a href="#/active">Active</a>
+          </li>
+          <li>
+            <a href="#/completed">Completed</a>
+          </li>
+        </ul>
+        <button className="clear-completed">Clear completed</button>
+      </footer>
+
+    </section>
+    <footer className="info">
+      <p>Double-click to edit a todo</p>
+      <p>Created by <a href="http://todomvc.com">Ysabel</a></p>
+      <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+    </footer>
     </>
   )
 }
