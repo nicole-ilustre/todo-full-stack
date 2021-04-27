23beed4cb3707b6bfd1527c5f05e002ca3e8d198
Caleb Ion <calebionpersonal@gmail.com>
2021-04-27 17:11:10 +1200

    Add list todos route

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -3,7 +3,17 @@ const router = express.Router()
 
 const { addTodo, getTodos, getTodo, deleteTodo, updateTodo } = require('../db/db')
 
-//gettodos
+router.get('/', (req, res) => {
+  getTodos()
+    .then((todos) => {
+      res.json(todos).sendStatus(200)
+      return null
+    })
+    .catch((e) => {
+      res.sendStatus(500)
+      console.log(e.message)
+    })
+})
 
 //addtodo
 
